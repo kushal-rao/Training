1e7b03840159afc031e9b354662d9fda46c343b7
Kushal Rao <kushalrao@Kushals-M4-MacBook-Air.local>
2026-03-25 16:58:21 -0500

    Made Template

---
+++ template.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <string>
+#include <sstream>
+#include <cmath>
+#include <map>
+#include <set>
+#include <limits>
+#include <tuple>
+using namespace std;
+
+// Type Aliases
+using ll = long long;
+using vi = vector<int>;
+using pii = pair<int, int>;
+
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+
+// Program Logic
+void solve() {
+   
+}
+
+int main() {
+    // Optimize standard I/O operations for speed
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    // cin >> t; // UNCOMMENT THIS LINE if the problem specifies multiple test cases
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
